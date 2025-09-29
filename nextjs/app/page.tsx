959e6067897fbb0695b1475eac141b2b8b435c0d
Go-Morishita <go20040128@gmail.com>
2025-09-29 21:42:19 +0900

    update

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-3xl">
-        Hello, I&#39;m Go Morishita!!!
+        Hello, I&#39;m Go Morishita!!!!!!!!!
       </h1>
     </>
   );
